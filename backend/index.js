d7c9e83792e5c2cc87592f91af6e9add30e44357
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-09-30 17:58:10 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,16 @@ connectDB(); //Database connection
 app.use('/api/user',userRoutes);
 app.use("/api/blog",blogRoutes);
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build","index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build","index.html"));
+// });
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'))
+})
 
 // if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
 //   app.use(express.static('/client/build'));
