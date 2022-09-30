730f643242c810fb28266c318645d0ee6f6ea915
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-09-30 23:07:39 +0530

    updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://blogging-app-karan.herokuapp.com/",
     withCredentials: true,
 }); 
 
