55cc9b39d83363da632e79aae1e8f10c62b62279
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-09-30 22:22:16 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,10 @@ app.use("/api/blog",blogRoutes);
 //   res.sendFile(path.resolve(__dirname, "../client/build","index.html"));
 // });
 
-// app.use(express.static(path.join(__dirname, '../client/build')))
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build'))
-// })
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'))
+})
 
 // if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
 //   app.use(express.static('/client/build'));
@@ -49,9 +49,9 @@ app.use("/api/blog",blogRoutes);
 //   });
 //  }
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../client/build/index.html")));
-  }
+// if (process.env.NODE_ENV == "production") {
+//     app.use(express.static(path.resolve(__dirname, "../client/build")));
+//     app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../client/build/index.html")));
+//   }
 
 app.listen(PORT,()=>console.log("app is succesfully running on PORT no.",PORT)); //Server running on port 8080
